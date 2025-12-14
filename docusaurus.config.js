0d854ffc256a5075336bb6a5d2fb8fa539987e44
Jeambo <226494145+jeambos@users.noreply.github.com>
2025-12-14 23:31:30 +0800

    解决数学公式显示两次问题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,8 +96,7 @@ const config = {
     {
       href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
       type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3yUyH/6+ywqj',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
       crossOrigin: 'anonymous',
     },
   ],
